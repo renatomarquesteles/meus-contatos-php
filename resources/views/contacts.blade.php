311b2195dec78cdfade23d6160e009ea2aeed60f
Renato Marques <renatomarquesteles@gmail.com>
2020-06-10 17:33:27 -0300

    cria listagem de contatos

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -6,15 +6,41 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <div class="row no-gutters justify-content-end">
+                <p>
+                    <a class="btn btn-primary" href="/contacts/new" role="button">+ Criar Contato</a>
+                </p>
+            </div>
             <div class="card">
                 <div class="card-header">Lista de Contatos</div>
-
                 <div class="card-body">
-                    @forelse ($contacts as $contact)
-                        <p>{{ $contact->name }}</p>
-                    @empty
-                        <p>Nenhum contato encontrado.</p>
-                    @endforelse
+                    @isset($contacts)
+                    <table class="table table-borderless table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Telefone</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($contacts as $contact)
+                            <tr>
+                                <th scope="row">{{ $contact->id }}</th>
+                                <td>{{ $contact->name }}</td>
+                                <td>{{ $contact->email }}</td>
+                                <td>{{ $contact->phone }}</td>
+                                <td><a href="#">Editar</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    @endisset
+                    @empty($contacts)
+                    <p>Nenhum contato encontrado.</p>
+                    @endempty
                 </div>
             </div>
         </div>
