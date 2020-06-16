3962597b470eb49eddc2d278d216ef3e873ad1a3
Renato Marques <renatomarquesteles@gmail.com>
2020-06-16 14:01:48 -0300

    remove classes inutilizadas

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/contacts', 'ContactsController@index');
 Route::get('/contacts/new', 'ContactsController@create');
 Route::post('/contacts', 'ContactsController@store');
